fb9c353ff18911c10218dc61d6619f1348481794
bydeng <bydeng@ctrip.com>
2020-04-10 19:36:32 +0800

    update

--- by-log/src/main/java/com/by/AppStart.java
+++ by-log/src/main/java/com/by/AppStart.java
@@ -6,13 +6,8 @@ import org.slf4j.LoggerFactory;
 
 public class AppStart {
 
-    private static Logger logger = null;
-//    private static final Logger logger = Logger.getLogger(AppStart.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(AppStart.class.getName());
 
-    static {
-//        LogLog.setInternalDebugging(true);
-        logger = LoggerFactory.getLogger(AppStart.class);
-    }
 
     public static void main(String[] args) {
 
